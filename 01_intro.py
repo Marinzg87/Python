80fe20668d2a4e91e089aced47e14516a9e5b94f
Marcin Bogusz <bogusz.marcin@icloud.com>
2022-07-12 10:25:29 +0200

    first commit add 01_intro.py

---
+++ 01_intro.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# komentarz - dobre komentowanie kodu jest kluczowe
+# dzielenie kodu na komórki # %%
+
+# %% dodawania
+print(2 + 2)
+
+# %% mnożenie
+print(3 * 3)
+
+# %% wyrażenie matematyczne
+print(3 + 2 * 2)
+
+# %% dzielenie z resztą
+10 / 3
+4 / 2
+
+# %% dzielenie całkowite
+print(10 // 3)
+print(7 // 6)
+
+# %% potęgowanie
+print(2 ** 5)
+
+# %% dzielenie modulu - zwraca resztę z dzielenia
+print(10 % 3)
+print(12 % 5)
