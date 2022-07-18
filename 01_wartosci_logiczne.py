8c770d91cf06ee4e89e2c0a010edd1dba6e6cf4e
Marcin Bogusz <bogusz.marcin@icloud.com>
2022-07-18 21:43:27 +0200

    Kolejna sesja nauki

---
+++ 01_wartosci_logiczne.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 21:31:57 2022
+
+@author: marcinbogusz
+"""
+
+# %%
+val_1 = True
+val_2 = False
+
+# %%
+print(val_1)
+print(type(val_1))
+
+# %% koniunkcja - oba zdarzenia są prawdziwe
+True and True
+True and False
+
+# %% alternatywa - jest prawdziwa, gdy chociaż jeden element jest True
+True or True
+
+# %% negacja
+True
+not True
+False
+not False
+
+# %% bool - wartości logiczne
+bool('pyhon')
